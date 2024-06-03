0c5f2c32cb74705e04f97fcf28a02d3e0887ac79
Shikhar Singh Patel <1shikhar1@gmail.com>
2024-06-03 15:17:28 +0530

    initial commit

---
+++ src/main/java/com/community/connect/CommunityConnectApplication.java
@@ -0,0 +1,13 @@
+package com.community.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityConnectApplication.class, args);
+	}
+
+}
